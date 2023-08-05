65c1e887b1b142b8d01f0f89f2d2a69e970ef7e8
lxx <lxx5@meitu.com>
2023-08-05 21:37:45 +0800

    :sparkles: add ui dep

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 8080,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
