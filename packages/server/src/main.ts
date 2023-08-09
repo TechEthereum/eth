0f20a3ae78d757db0abfb1141e8253716e1013c2
lxx <lxx5@meitu.com>
2023-08-09 11:00:15 +0800

    :sparkles: (server) 新增参数校验

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+
+  // 集合 DTO 入参校验
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3003);
 }
--- packages/server/src/modules/admin/user/dto.ts
+++ packages/server/src/modules/admin/user/dto.ts
@@ -1,11 +1,23 @@
 import { UserModel } from '@/types';
 import { PartialType } from '@nestjs/mapped-types';
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class CreateAdminUserDto implements UserModel {
   id: string;
+
+  @IsEmail()
+  @IsNotEmpty({ message: '账号不能为空' })
+  @MaxLength(20, { message: '账号长度不能超过 20' })
+  @MinLength(6, { message: '账号长度不能少于 6' })
   username: string;
+
+  @IsNotEmpty({ message: '密码不能为空' })
+  @MaxLength(20, { message: '密码长度不能超过 20' })
+  @MinLength(6, { message: '密码长度不能少于 6' })
   password: string;
+
   createdAt: Date;
+
   updatedAt: Date;
 }
 
